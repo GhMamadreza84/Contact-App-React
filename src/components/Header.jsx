2e186f9215864c6daee7986eb3035cdb1ccdf986
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-16 15:33:14 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css"
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Contact App</h1>
+      <p><a href="#">GhMamadreza</a> | Create With 💙</p>
+    </div>
+  );
+};
+
+export default Header;
