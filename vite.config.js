261a8b651be3df7331132a5f8a96915fd955bb24
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-22 16:05:15 +0300

    change base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ghmamadreza84.github.io/Contact-App-React/",
+  base: './src/'
 })
